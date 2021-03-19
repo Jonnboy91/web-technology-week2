68b17e7f1c5240a152142f285bee15af4ca547d9
Jonnboy91 <jon.nesten@gmail.com>
2021-03-19 10:03:41 +0200

    cat routes (get, post, put, delete) and html with get and post

--- nodejs-demo/index.js
+++ nodejs-demo/index.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World test!');
+});
+
+app.get('/cat', (req, res) => {
+  console.log('get cat');
+  res.send('Hello cat!');
+});
+
+app.post('/cat', (req, res) => {
+  console.log('post cat');
+  res.send('post cat');
+});
+
+app.put('/cat', (req, res) => {
+  console.log('put cat');
+  res.send('put cat');
+});
+
+app.delete('/cat', (req, res) => {
+  console.log('delete cat');
+  res.send('delete cat');
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
