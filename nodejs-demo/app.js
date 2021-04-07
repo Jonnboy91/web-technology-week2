12252f3103dae52549f7b676efed4da80bbb9b48
Jonnboy91 <jon.nesten@gmail.com>
2021-04-07 12:10:14 +0300

    installed passport-jwt, and worked with JSON Web Token Strategy

--- nodejs-demo/app.js
+++ nodejs-demo/app.js
@@ -3,11 +3,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const catRouter = require('./routes/catRouter');
-const userRouter = require('./routes/userRouter');
+const catRoute = require('./routes/catRoute');
+const userRoute = require('./routes/userRoute');
+const passport = require('./utils/pass');
+const authRoute = require('./routes/authRoute')
 
 app.use(express.static('public')); // Define public folder
-app.use(express.static('uploads')); // Define public folder
+app.use(express.static('uploads')); // Define uploads folder
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -16,8 +18,9 @@ app.get('/', (req, res) => {
   res.send('Hello World test!');
 });
 
-app.use('/cat', catRouter);
-app.use('/user', userRouter);
+app.use('/auth', authRoute);
+app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
+app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- nodejs-demo/models/userModel.js
+++ nodejs-demo/models/userModel.js
@@ -28,9 +28,21 @@ const getUserById = async(id) =>{
   }
 }
 
+const getUserLogin = async (params) => {
+  try {
+    console.log(params);
+    const [rows] = await promisePool.execute(
+        'SELECT * FROM wop_user WHERE email = ?;',
+        params);
+    return rows;
+  } catch (e) {
+    console.log('error', e.message);
+  }
+};
 
 module.exports = {
   getAllUsers,
   insertUser,
   getUserById,
+  getUserLogin,
 };
