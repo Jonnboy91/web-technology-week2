a44d148e9afce9347414ed2691363cacf22a1966
Jonnboy91 <jon.nesten@gmail.com>
2021-04-09 10:54:00 +0300

    app.js with https ready for both localhost and production servers

--- nodejs-demo/app.js
+++ nodejs-demo/app.js
@@ -1,8 +1,18 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
+
 const app = express();
-const port = 3000;
+const port = process.env.HTTP_PORT || 3000;
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'production') {
+  require('./utils/production')(app, port);
+} else {
+  require('./utils/localhost')(app, process.env.HTTPS_PORT || 8000, port);
+}
+
 const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
 const passport = require('./utils/pass');
@@ -22,6 +32,6 @@ app.use('/auth', authRoute);
 app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
 app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
